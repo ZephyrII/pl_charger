745fdbf4b8df750c8ff93cc8ab25b40ef332ca1f
ZephyrII <tomasz.no@pm.me>
2021-08-19 23:32:52 +0200

    logging

--- train_kpts.py
+++ train_kpts.py
@@ -17,10 +17,13 @@ from typing import Optional
 import torch
 from torch.nn import functional as F
 from torch.utils.data import DataLoader, random_split
+import cv2
+import numpy as np
 
 import pytorch_lightning as pl
 from pytorch_lightning.utilities.cli import LightningCLI
 from pytorch_lightning.utilities.imports import _TORCHVISION_AVAILABLE
+from pytorch_lightning.loggers import NeptuneLogger
 from adas_dataset import AdasDataset
 
 if _TORCHVISION_AVAILABLE:
@@ -41,7 +44,6 @@ class ChargerKpts(pl.LightningModule):
         self.batch_size = 1 #FIXME
         self.save_hyperparameters(ignore=['backbone'])
         self.num_points = 4
-        # self.backbone = models.resnet50(pretrained=True)
         self.backbone = resnet_fpn_backbone('resnet50', pretrained=True)
         self.kpt_head = torch.nn.Sequential(
             torch.nn.Conv2d(256, 512, 3, padding=1),
@@ -65,13 +67,6 @@ class ChargerKpts(pl.LightningModule):
             torch.nn.ConvTranspose2d(256, 128, 3, stride=2, padding=1),
             torch.nn.BatchNorm2d(128),
             torch.nn.ConvTranspose2d(128, 64, 3, stride=2, output_padding=1),
-            # torch.nn.Conv2d(256, 64, 3, padding=1),
-            # torch.nn.BatchNorm2d(64),
-            # torch.nn.Upsample(scale_factor=2),
-            # torch.nn.Conv2d(256, 128, 3, padding=1),
-            # torch.nn.BatchNorm2d(128),
-            # torch.nn.Upsample(scale_factor=2),
-            # torch.nn.Conv2d(128, 64, 3, padding=1),
             torch.nn.BatchNorm2d(64),
             torch.nn.Conv2d(64, self.num_points, 3, padding=1),
             torch.nn.Sigmoid(),
@@ -92,6 +87,7 @@ class ChargerKpts(pl.LightningModule):
         assert hm.shape == y.shape, f"hm and target shape mismatch. hm:{hm.shape()}, y:{y.shape()}"
         loss = F.binary_cross_entropy(hm, y)
         self.log('train_loss', loss, on_epoch=True)
+        self.logger.experiment.log_metric("train_loss", loss)
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -101,14 +97,32 @@ class ChargerKpts(pl.LightningModule):
         hm = self.kpt_head(fm)
         loss = F.binary_cross_entropy(hm, y)
         self.log('valid_loss', loss, on_step=True)
+        self.logger.experiment.log_metric("val_loss", loss)
+
+        if batch_idx==0:
+            input = x[0].cpu().numpy()
+            input = np.transpose(input, [1,2,0])
+
+            res = np.sum(hm[0].cpu().numpy(), axis=0)
+            res = res-np.min(res)
+            res = res/np.max(res)
+            mask = np.zeros_like(input)
+            mask[:,:,0] = res
+            alpha = 0.5
+            out_img = cv2.addWeighted(input, alpha, mask, 1-alpha, 0)
+
+            self.logger.experiment.log_image("test_img", out_img)
 
     def test_step(self, batch, batch_idx):
         x, y = batch
         fm = self.backbone(x)
         assert fm.shape()[-2:] == (240, 240), f"incorrect fm shape:{fm.shape()}"
         hm = self.kpt_head(fm)
-        loss = F.binary_cross_entropy(hm, y)
-        self.log('test_loss', loss)
+        res = np.sum(hm, axis=0)
+        alpha = 0.5
+        out_img = cv2.addWeighted(res, alpha, x, 1-alpha, 0)
+
+        self.logger.experiment.log_image("test_img", out_img)
 
     def predict_step(self, batch, batch_idx, dataloader_idx=None):
         x, y = batch
@@ -118,17 +132,23 @@ class ChargerKpts(pl.LightningModule):
         return torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
 
     def train_dataloader(self):
-        return DataLoader(AdasDataset(self.ds_path, "train"), batch_size=self.batch_size, num_workers=3)
+        return DataLoader(AdasDataset(self.ds_path, "train"), batch_size=self.batch_size, num_workers=2, pin_memory=True)
 
     def val_dataloader(self):
-        return DataLoader(AdasDataset(self.ds_path, "val"), batch_size=self.batch_size, num_workers=3)
+        return DataLoader(AdasDataset(self.ds_path, "val"), batch_size=self.batch_size, num_workers=2, pin_memory=True)
 
     def test_dataloader(self):
-        return DataLoader(AdasDataset(self.ds_path, "val"), batch_size=self.batch_size, num_workers=3)
+        return DataLoader(AdasDataset(self.ds_path, "val"), batch_size=self.batch_size, num_workers=2, pin_memory=True)
 
 if __name__ == '__main__':
+    neptune_logger = NeptuneLogger(
+    api_key="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI4NGRmMmFkNi0wMWNjLTQxY2EtYjQ1OS01YjQ0YzRkYmFlNGIifQ==",  # replace with your own
+    project_name="tnowak/charger",
+    experiment_name="res50",  
+    # experiment_id='CHAR-1',
+)
     # dataset = ChargerData("/root/share/tf/dataset/4_point_final/")
-    ch_call = pl.callbacks.ModelCheckpoint(save_last=True)
-    trainer = pl.Trainer(gpus=1, checkpoint_callback=True, callbacks=[ch_call], accumulate_grad_batches=4)
-    model = ChargerKpts("/root/share/tf/dataset/4_point_final/")
+    ch_call = pl.callbacks.ModelCheckpoint(save_last=True, dirpath="./checkpoints", every_n_epochs=1)
+    trainer = pl.Trainer(gpus=1, checkpoint_callback=True, callbacks=[ch_call], accumulate_grad_batches=4, logger=neptune_logger)
+    model = ChargerKpts("/root/share/tf/dataset/final_localization/tape_1.0/")
     trainer.fit(model)
